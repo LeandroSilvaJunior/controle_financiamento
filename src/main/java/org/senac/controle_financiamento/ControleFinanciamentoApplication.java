bba5197ef955b32824aca3633d598fb0d424a472
Leandro da Silva Junior <leandro.sj.dev@gmail.com>
2019-11-25 18:53:38 -0300

    Initial commit

---
+++ src/main/java/org/senac/controle_financiamento/ControleFinanciamentoApplication.java
@@ -0,0 +1,13 @@
+package org.senac.controle_financiamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinanciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFinanciamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/senac/controle_financiamento/ControleFinanciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senac.controle_financiamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
