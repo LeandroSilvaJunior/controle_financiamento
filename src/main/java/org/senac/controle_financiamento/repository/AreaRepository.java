62cb04628925266430d16df8ea366f04028a7d18
Leandro da Silva Junior <leandro.sj.dev@gmail.com>
2019-11-25 19:36:11 -0300

    Criar as interfaces repository da aplicação

---
+++ src/main/java/org/senac/controle_financiamento/repository/AreaRepository.java
@@ -0,0 +1,7 @@
+package org.senac.controle_financiamento.repository;
+
+import org.senac.controle_financiamento.domain.Area;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AreaRepository extends JpaRepository<Area, Integer> {
+}
---
+++ src/main/java/org/senac/controle_financiamento/repository/ProjetoRepository.java
@@ -0,0 +1,7 @@
+package org.senac.controle_financiamento.repository;
+
+import org.senac.controle_financiamento.domain.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjetoRepository extends JpaRepository<Projeto, Integer> {
+}
