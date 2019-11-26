3ad40e4f9519d0808e617310e768749ed2512015
Leandro da Silva Junior <leandro.sj.dev@gmail.com>
2019-11-25 21:52:58 -0300

    commit final

--- src/main/java/org/senac/controle_financiamento/controller/ProjetoController.java
+++ src/main/java/org/senac/controle_financiamento/controller/ProjetoController.java
@@ -1,13 +1,13 @@
 package org.senac.controle_financiamento.controller;
 
+import org.senac.controle_financiamento.domain.Area;
 import org.senac.controle_financiamento.domain.Projeto;
+import org.senac.controle_financiamento.repository.AreaRepository;
 import org.senac.controle_financiamento.repository.ProjetoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -16,6 +16,53 @@ import javax.validation.Valid;
 public class ProjetoController {
 
     @Autowired
-    private ProjetoRepository repository;
+    private ProjetoRepository projetoRepository;
 
+    @Autowired
+    private AreaRepository areaRepository;
+
+    @PostMapping
+    public ResponseEntity<?> insert(@RequestBody @Valid Projeto projeto) {
+        ResponseEntity<?> responseEntity = validarProjeto(projeto);
+
+        if (responseEntity == null) {
+            responseEntity = ResponseEntity.ok(projetoRepository.save(projeto));
+        }
+
+        return responseEntity;
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody @Valid Projeto projeto) {
+        Projeto projetoPersistido = projetoRepository.findOneByNome(projeto.getNome());
+
+        if (projetoPersistido == null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(String.format(" O Projeto %s não esta cadastrado!", projeto.getNome()));
+        } else {
+            ResponseEntity<?> responseEntity = validarProjeto(projetoPersistido);
+
+            if (responseEntity == null) {
+                responseEntity = ResponseEntity.ok(projetoRepository.save(projetoPersistido));
+            }
+
+            return responseEntity;
+        }
+    }
+
+    private ResponseEntity<?> validarProjeto(Projeto projeto) {
+        Area area = areaRepository.findByNomeIgnoreCase(projeto.getArea().getNome());
+        if (area == null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(String.format(" A Área %s não esta cadastrada!", projeto.getArea().getNome()));
+        } else {
+            projeto.setArea(area);
+            if (projeto.getOrcamento().compareTo(area.getOrcamentoMaximo()) == 1 &&
+                    projeto.getOrcamento().compareTo(area.getOrcamentoMinimo()) == -1) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body("O Orçamento do projeto precisa estar dentro dos limites estabelecidos pela área.");
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/org/senac/controle_financiamento/repository/ProjetoRepository.java
+++ src/main/java/org/senac/controle_financiamento/repository/ProjetoRepository.java
@@ -4,4 +4,6 @@ import org.senac.controle_financiamento.domain.Projeto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjetoRepository extends JpaRepository<Projeto, Integer> {
+
+    public Projeto findOneByNome(String nome);
 }
